9129873f2b0716e4c36922a5f40f6f985f8379ec
Cross-companion <mickdaniels101@gmail.com>
2023-12-01 10:56:53 +0100

    Re-enabled multer image storing.

--- controllers/user/usersController.js
+++ controllers/user/usersController.js
@@ -1,7 +1,49 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../../models/user/userModel');
+const { multerStorage, multerFilter } = require('./helpers');
 const factory = require('../handlerFactory');
 const AppError = require('../../utilities/AppError');
 const catchAsync = require('../../utilities/catchAsync');
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadProfileImages = upload.fields([
+  { name: 'profileImage', maxCount: 1 },
+  { name: 'profileCoverImage', maxCount: 1 },
+]);
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  const { profileImage, profileCoverImage } = req.files;
+  if (!profileImage && !profileCoverImage) return next();
+
+  if (profileImage) {
+    req.body.profileImage = `${process.env.PROFILE_IMAGE_PREFIX}-${
+      req.user._id
+    }-${Date.now()}.jpeg`;
+    sharp(req.files.profileImage[0].buffer)
+      .resize(500, 500)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/imgs/users/${req.body.profileImage}`);
+  }
+
+  if (profileCoverImage) {
+    req.body.profileCoverImage = `${process.env.PROFILE_COVER_IMAGE_PREFIX}-${
+      req.user._id
+    }-${Date.now()}.jpeg`;
+    sharp(req.files.profileCoverImage[0].buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/imgs/users/${req.body.profileCoverImage}`);
+  }
+
+  next();
+});
 
 exports.getAllUsers = factory.findAll(User);
 
