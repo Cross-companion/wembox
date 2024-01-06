babe4cf9a5d220ec72a78b727260ee46ee9b604b
Cross-companion <mickdaniels101@gmail.com>
2024-01-06 17:08:58 +0100

    Deploy bug fix attempt 2 - redis.

--- utilities/redisInit.js
+++ utilities/redisInit.js
@@ -3,11 +3,11 @@ const Redis = require('ioredis');
 const { REDIS_PORT, REDIS_HOST, NODE_ENV, REDIS_URL } = process.env;
 const redis = new Redis(
   NODE_ENV === 'production'
-    ? {
+    ? REDIS_URL
+    : {
         host: REDIS_HOST,
         port: REDIS_PORT,
       }
-    : REDIS_URL
 );
 
 console.log(
