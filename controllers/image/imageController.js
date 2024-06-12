574efa4ae419e5a51863327fa74bff9dacb960f7
Cross-companion <mickdaniels101@gmail.com>
2024-06-13 00:21:20 +0100

    Image sending finising touches.

--- controllers/image/imageController.js
+++ controllers/image/imageController.js
@@ -4,11 +4,19 @@ const catchAsync = require('../../utilities/catchAsync');
 
 const { AWS_IMAGE_NOT_FOUND } = process.env;
 
+async function getNotFoundImg(imageManager) {
+  const imgNum = Math.round(Math.random()) + 1;
+  const notFoundImgUrl = AWS_IMAGE_NOT_FOUND.replace('<NUM>', imgNum);
+  const ImageData = await imageManager.getFromAWS(notFoundImgUrl);
+  return ImageData;
+}
+
 exports.getImage = catchAsync(async (req, res, next) => {
   let { folderName, imagePath } = req.params;
   if (!folderName || !imagePath) return next(new AppError('Invalid img path.'));
   const imageUrl = `${folderName}/${imagePath}`;
   const imageManager = new ImageFile();
+
   try {
     const { Body: image, ContentType } = await imageManager.getFromAWS(
       imageUrl
@@ -16,12 +24,15 @@ exports.getImage = catchAsync(async (req, res, next) => {
     res.setHeader('Content-Type', ContentType);
     image.pipe(res);
   } catch (err) {
-    const imgNum = Math.round(Math.random()) + 1;
-    const notFoundImgUrl = AWS_IMAGE_NOT_FOUND.replace('<NUM>', imgNum);
-    const { Body: image, ContentType } = await imageManager.getFromAWS(
-      notFoundImgUrl
-    );
+    const { Body: image, ContentType } = await getNotFoundImg(imageManager);
     res.status(404).setHeader('Content-Type', ContentType);
     image.pipe(res);
   }
+});
+
+exports.showNotFoundImage = catchAsync(async (req, res) => {
+  const imageManager = new ImageFile();
+  const { Body: image, ContentType } = await getNotFoundImg(imageManager);
+  res.status(404).setHeader('Content-Type', ContentType);
+  image.pipe(res);
 });
--- routers/imageRoutes.js
+++ routers/imageRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('../controllers/authController');
 const imageController = require('../controllers/image/imageController');
 
 router.route('/:folderName/:imagePath').get(imageController.getImage);
-// router.use(authController.protect);
+router
+  .route('/:folderName/:imagePath/*')
+  .get(imageController.showNotFoundImage);
+router.route('/*').get(imageController.showNotFoundImage);
+
 //-- <> -- //
 module.exports = router;
