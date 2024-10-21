0ff366b0015f2272e0940b2c4d3502b3da59a66d
Cross-companion <mickdaniels101@gmail.com>
2024-10-21 16:38:30 +0100

    Added trim cache

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,88 @@
-// sw.js
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-  // You can cache assets here if needed
+const sw_version = 'v1';
+const app_cache = `wembox-${sw_version}`;
+
+const sw_caches = {
+  assets: {
+    name: `${sw_version}assets`,
+  },
+  images: {
+    name: `${sw_version}images`,
+    limit: 70,
+  },
+  pages: {
+    name: `${sw_version}pages`,
+    limit: 10,
+  },
+};
+
+const preinstall = [
+  '/',
+  '/Imgs/app-images/logo-30x30.png',
+  '/CSS/style.css',
+  '/js/utils/socket.io.js',
+  '/js/controllers/appController.js',
+  // '/offline_page',
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(sw_caches.assets.name).then(async function (cache) {
+      return cache.addAll(preinstall);
+    })
+  );
 });
 
 self.addEventListener('activate', (event) => {
   console.log('Service Worker activating.');
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => {
+        return Promise.all(
+          keys
+            .filter((key) => {
+              return !key.startsWith(version);
+            })
+            .map((key) => {
+              return caches.delete(key);
+            })
+        );
+      })
+      .then(() => clients.claim())
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url);
+self.addEventListener('fetch', async (event) => {
+  const destination = event.request.destination;
+  // console.log(destination, event.request.url);
+  // const res = await app_cache.match('/CSS/style.css');
+  // console.log(res);
   // You can handle requests here
 });
+
+self.addEventListener('message', (messageEvent) => {
+  if (messageEvent.data == 'clean up') {
+    for (let key in sw_caches) {
+      if (sw_caches[key].limit != undefined) {
+        trimCache(sw_caches[key].name, sw_caches[key].limit);
+      }
+    }
+  }
+});
+
+function trimCache(cache_name, limit) {
+  caches.open(cache_name).then((cache) => {
+    cache.keys().then((items) => {
+      if (items.length > limit) {
+        (async function () {
+          let i = 0,
+            end = items.length - limit;
+          while (i < end) {
+            console.log('deleting item', i, items[i]);
+            cache.delete(items[i++]);
+          }
+        })();
+      }
+    });
+  });
+}
