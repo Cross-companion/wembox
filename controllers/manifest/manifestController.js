ab58107f5799bf03aa435dce6c66492265ae62ed
Cross-companion <mickdaniels101@gmail.com>
2024-11-26 18:44:08 +0100

    Manifest chat shortcut implemented

--- controllers/manifest/manifestController.js
+++ controllers/manifest/manifestController.js
@@ -1,4 +1,6 @@
+const Contact = require('../../models/contact/contactsModel');
 const catchAsync = require('../../utilities/catchAsync');
+const { getContactsQuery } = require('../contact/helper');
 const { populateManifest } = require('./helper');
 
 exports.getAuthManifest = catchAsync(async (req, res, next) => {
@@ -7,6 +9,41 @@ exports.getAuthManifest = catchAsync(async (req, res, next) => {
 });
 
 exports.getAppManifest = catchAsync(async (req, res, next) => {
-  const manifest = populateManifest();
-  res.json(manifest);
+  try {
+    const userId = req.user._id;
+    const maxShortcuts = 3;
+    const skipBy = 0;
+    const contacts = await getContactsQuery(
+      { users: userId },
+      { userID: userId, maxShortcuts, skipBy }
+    );
+    const shortcuts = contacts.map((contact) => {
+      const {
+        otherUser: { name, profileImage, note, frontEndUsername },
+        _id: contactId,
+      } = contact;
+
+      return {
+        name,
+        url: `/?chats=${contactId}`,
+        description:
+          note || `Chat with ${name}(@${frontEndUsername}) on wembox inc`,
+        icons: [
+          {
+            src: `${req.protocol}://${req.get('host')}${
+              profileImage.startsWith('/') ? profileImage : '/' + profileImage
+            }`,
+            type: 'image/png',
+            sizes: '500x500',
+          },
+        ],
+      };
+    });
+    const manifest = populateManifest(shortcuts);
+    res.json(manifest);
+  } catch (err) {
+    console.error(err);
+    const BASE_MANIFEST = populateManifest();
+    res.json(BASE_MANIFEST);
+  }
 });
